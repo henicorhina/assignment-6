02d291c4cab4d92a640c719ee5d518b5f047e05e
OSCAR JOHNSON <OJOHNS7@TIGERS.LSU.EDU>
2016-02-10 19:50:11 -0600

    still in progress

--- answers/henicorhina/task5.py
+++ answers/henicorhina/task5.py
@@ -3,48 +3,49 @@
 
 """
 BIOL7800 assignment 6
-Oscar Johnson 9 February 2016
+Oscar Johnson 10 February 2016
 """
 
 import argparse
 import math
 import sys
 
-sys.setrecursionlimit(10000)
+sys.setrecursionlimit(1000)
 
 
 def get_args():
     parser = argparse.ArgumentParser(
-                description="use recursion to estimate Euler's e"
+                description="""use recursion to estimate Euler's e"""
                 )
     
     parser.add_argument(
-                "-- recursion_depth",
-                dest = "recursion_depth",
+                "recursion_depth",
+                help = "the maximum number of times over which to run the recursive function",
                 type = int
                 )
+    #get_args.recursion_depth
     return parser.parse_args()
 
-def e_recursive_estimator(n = 0, recursion_depth = 0):
+def e_recursive_estimator(args, n = 0):
 
     """
     estimates Euler's e using recursion
     """
-    limit = 0
-    if n == 0:
-        return (1/math.factorial(n)) + e_recursive_estimator(n+1, recursion_depth)
+    #limit = 0
+    args = int(get_args())
+    #args = 100    
+    if n < args:
+        return (1/math.factorial(n)) + e_recursive_estimator(args, n+1)
  
-    elif n <= limit:
-        return (1/math.factorial(n)) + e_recursive_estimator(n+1, limit)
-        
     else:
         return 0
 
+#print (e_recursive_estimator())
 
 def main():
     args = get_args()
-    print (e_recursive_estimator(args))
-
+    print (e_recursive_estimator(args, 0))
+    #pass
 
 if __name__ == '__main__':
     main()
